7b5fac9031ce52ff0e7f8f4c436517e19910fb2f
manasvisharma1 <manasvi_s@ymail.com>
2024-06-02 18:27:52 +0530

    Changes

---
+++ DBOperation/DBOperation/Data/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DBOperation.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { 
+        
+        }
+    }
+}
